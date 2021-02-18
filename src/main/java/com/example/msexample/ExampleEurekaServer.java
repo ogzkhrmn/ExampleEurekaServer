aecd5ddb835f96fd3d61bfa058aacdccb3fbb74e
oguzkahraman <oguz.kahraman@optdcom.net>
2021-02-18 23:32:13 +0300

    Initial message

--- src/main/java/com/example/msexample/ExampleEurekaServer.java
+++ src/main/java/com/example/msexample/ExampleEurekaServer.java
@@ -3,14 +3,13 @@ package com.example.msexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MsexampleApplication {
+public class ExampleEurekaServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsexampleApplication.class, args);
+        SpringApplication.run(ExampleEurekaServer.class, args);
     }
 
 }
